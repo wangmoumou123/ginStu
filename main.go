5f46d1e510675532857b2dbed607f1b4bde6e863
王 升 <1939098193@qq.com>
2024-05-06 14:16:59 +0800

    day_24_5_6

--- main.go
+++ main.go
@@ -6,17 +6,28 @@ import (
 	"net/http"
 )
 
+type Article struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
 func main() {
-	g := gin.Default()
-	g.GET("/", func(c *gin.Context) {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/**/*")
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	g.GET("/tt", func(c *gin.Context) {
+	r.GET("/jsonp", func(c *gin.Context) {
+		c.JSONP(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/tt", func(c *gin.Context) {
 		c.String(http.StatusOK, "你是: %v 大 大4554声道", "爱惜")
 	})
-	g.POST("/post_test", func(c *gin.Context) {
+	r.POST("/post_test", func(c *gin.Context) {
 		p := c.Query("p")
 
 		fmt.Println(p)
@@ -25,8 +36,24 @@ func main() {
 		})
 
 	})
-	err := g.Run()
+	r.GET("/index", func(c *gin.Context) {
+		art := Article{"123", "456789"}
+		c.HTML(http.StatusOK, "default/index.html", gin.H{
+			"title": "hello",
+			"art":   art,
+		})
+	})
+	r.GET("/a_index", func(c *gin.Context) {
+		art := Article{"123", "456789"}
+		c.HTML(http.StatusOK, "admin/index.tmpl", gin.H{
+			"title": "hello",
+			"art":   art,
+		})
+	})
+
+	err := r.Run()
 	if err != nil {
+
 		return
 	} // 监听并在 0.0.0.0:8080 上启动服务
 }
